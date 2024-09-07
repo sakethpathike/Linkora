15b4d5268440a26465246760a30da0858612f455
Saketh Pathike <sakethpathike@gmail.com>
2024-09-07 17:35:31 +0530

    feat(link-views): Update StaggeredViewComponent

--- app/src/main/java/com/sakethh/linkora/ui/commonComposables/link_views/components/StaggeredViewComponent.kt
+++ app/src/main/java/com/sakethh/linkora/ui/commonComposables/link_views/components/StaggeredViewComponent.kt
@@ -1,7 +1,11 @@
 package com.sakethh.linkora.ui.commonComposables.link_views.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +20,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,7 +29,9 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sakethh.linkora.ui.commonComposables.link_views.LinkUIComponentParam
+import com.sakethh.linkora.ui.commonComposables.pulsateEffect
 import com.sakethh.linkora.ui.screens.CoilImage
+import com.sakethh.linkora.utils.fadedEdges
 
 @Composable
 fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
@@ -32,20 +39,60 @@ fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
         Column(
             Modifier
                 .fillMaxWidth()
+                .clickable(onClick = {
+                    linkUIComponentParam.onLinkClick()
+                }, interactionSource = remember {
+                    MutableInteractionSource()
+                }, indication = null)
+                .pulsateEffect()
                 .padding(4.dp)
                 .clip(RoundedCornerShape(5.dp))
                 .border(
                     width = 1.dp,
-                    color = MaterialTheme.colorScheme.primary,
+                    color = MaterialTheme.colorScheme.secondary.copy(0.5f),
                     shape = RoundedCornerShape(5.dp)
                 )
         ) {
             if (linkUIComponentParam.imgURL.trim().isNotBlank()) {
-                CoilImage(
+                Box(modifier = Modifier.fillMaxSize()) {
+                    CoilImage(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .fadedEdges(colorScheme),
+                        imgURL = linkUIComponentParam.imgURL,
+                        contentScale = if (linkUIComponentParam.imgURL.startsWith("https://pbs.twimg.com/profile_images/")) ContentScale.Crop else ContentScale.Fit
+                    )
+                    Text(
+                        text = linkUIComponentParam.webBaseURL,
+                        modifier = Modifier
+                            .align(Alignment.BottomStart)
+                            .padding(
+                                start = 9.dp,
+                                top = 10.dp,
+                            )
+                            .background(
+                                color = MaterialTheme.colorScheme.primary.copy(0.15f),
+                                shape = RoundedCornerShape(5.dp)
+                            )
+                            .padding(5.dp),
+                        style = MaterialTheme.typography.titleLarge,
+                        fontSize = 10.sp
+                    )
+                }
+            } else {
+                Text(
+                    text = linkUIComponentParam.webBaseURL,
                     modifier = Modifier
-                        .fillMaxSize(),
-                    imgURL = linkUIComponentParam.imgURL,
-                    contentScale = if (linkUIComponentParam.imgURL.startsWith("https://pbs.twimg.com/profile_images/")) ContentScale.Crop else ContentScale.Fit
+                        .padding(
+                            top = 10.dp, start = 9.dp
+                        )
+                        .background(
+                            color = MaterialTheme.colorScheme.primary.copy(0.15f),
+                            shape = RoundedCornerShape(5.dp)
+                        )
+                        .padding(5.dp),
+                    style = MaterialTheme.typography.titleLarge,
+                    fontSize = 10.sp
                 )
             }
             Row(
