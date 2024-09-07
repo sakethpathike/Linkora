df0a4c2f7c18e72c555c5596ebd25f42f9bcc5fb
Saketh Pathike <sakethpathike@gmail.com>
2024-09-07 17:51:33 +0530

    feat(link-views): Update StaggeredViewComponent UI on long press

--- app/src/main/java/com/sakethh/linkora/ui/commonComposables/link_views/components/StaggeredViewComponent.kt
+++ app/src/main/java/com/sakethh/linkora/ui/commonComposables/link_views/components/StaggeredViewComponent.kt
@@ -1,8 +1,10 @@
 package com.sakethh.linkora.ui.commonComposables.link_views.components
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,10 +12,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -33,17 +37,20 @@ import com.sakethh.linkora.ui.commonComposables.pulsateEffect
 import com.sakethh.linkora.ui.screens.CoilImage
 import com.sakethh.linkora.utils.fadedEdges
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
     val colorScheme = MaterialTheme.colorScheme
         Column(
             Modifier
                 .fillMaxWidth()
-                .clickable(onClick = {
+                .combinedClickable(onClick = {
                     linkUIComponentParam.onLinkClick()
                 }, interactionSource = remember {
                     MutableInteractionSource()
-                }, indication = null)
+                }, indication = null, onLongClick = {
+                    linkUIComponentParam.onLongClick()
+                })
                 .pulsateEffect()
                 .padding(4.dp)
                 .clip(RoundedCornerShape(5.dp))
@@ -52,8 +59,24 @@ fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
                     color = MaterialTheme.colorScheme.secondary.copy(0.5f),
                     shape = RoundedCornerShape(5.dp)
                 )
+                .then(
+                    if (linkUIComponentParam.isSelectionModeEnabled.value) Modifier.background(
+                        colorScheme.primaryContainer
+                    ) else Modifier
+                )
+                .animateContentSize()
         ) {
-            if (linkUIComponentParam.imgURL.trim().isNotBlank()) {
+            if (linkUIComponentParam.isItemSelected.value) {
+                Box(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(150.dp)
+                        .background(MaterialTheme.colorScheme.primary),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(Icons.Default.CheckCircle, null, tint = colorScheme.onPrimary)
+                }
+            } else if (linkUIComponentParam.imgURL.trim().isNotBlank()) {
                 Box(modifier = Modifier.fillMaxSize()) {
                     CoilImage(
                         modifier = Modifier
@@ -67,8 +90,7 @@ fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
                         modifier = Modifier
                             .align(Alignment.BottomStart)
                             .padding(
-                                start = 9.dp,
-                                top = 10.dp,
+                                start = 9.dp, top = 10.dp, end = 9.dp
                             )
                             .background(
                                 color = MaterialTheme.colorScheme.primary.copy(0.15f),
@@ -76,7 +98,9 @@ fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
                             )
                             .padding(5.dp),
                         style = MaterialTheme.typography.titleLarge,
-                        fontSize = 10.sp
+                        fontSize = 10.sp,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
                     )
                 }
             } else {
@@ -84,7 +108,7 @@ fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
                     text = linkUIComponentParam.webBaseURL,
                     modifier = Modifier
                         .padding(
-                            top = 10.dp, start = 9.dp
+                            top = 10.dp, start = 9.dp, end = 9.dp
                         )
                         .background(
                             color = MaterialTheme.colorScheme.primary.copy(0.15f),
@@ -92,7 +116,9 @@ fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
                         )
                         .padding(5.dp),
                     style = MaterialTheme.typography.titleLarge,
-                    fontSize = 10.sp
+                    fontSize = 10.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
             Row(
@@ -102,7 +128,7 @@ fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
                     .fillMaxWidth()
                     .wrapContentHeight()
             ) {
-                Column(modifier = Modifier.fillMaxWidth(0.75f)) {
+                Column(modifier = Modifier.fillMaxWidth(if (linkUIComponentParam.isSelectionModeEnabled.value) 1f else 0.75f)) {
                     Text(
                         text = linkUIComponentParam.title,
                         modifier = Modifier
@@ -112,14 +138,14 @@ fun StaggeredViewComponent(linkUIComponentParam: LinkUIComponentParam) {
                         fontSize = 12.sp
                     )
                 }
-                IconButton(
-                    onClick = {
+                if (!linkUIComponentParam.isSelectionModeEnabled.value) {
+                    IconButton(onClick = {
                         linkUIComponentParam.onMoreIconClick()
+                    }) {
+                        Icon(
+                            Icons.Default.MoreVert, contentDescription = null
+                        )
                     }
-                ) {
-                    Icon(
-                        Icons.Default.MoreVert, contentDescription = null
-                    )
                 }
             }
         }
